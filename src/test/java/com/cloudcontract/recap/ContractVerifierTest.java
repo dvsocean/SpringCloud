c4dcf5bb83206eb39fc3994462ac3d3c887c8707
Ocean <dvsocean@icloud.com>
2018-06-03 13:02:35 -0700

    checkpoint

--- src/test/java/com/cloudcontract/recap/ContractVerifierTest.java
+++ src/test/java/com/cloudcontract/recap/ContractVerifierTest.java
@@ -6,31 +6,38 @@ import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import io.restassured.module.mockmvc.response.MockMvcResponse;
 import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
 import io.restassured.response.ResponseOptions;
 import org.junit.Test;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 
+@AutoConfigureStubRunner
 public class ContractVerifierTest extends MvcTest {
 
   @Test
   public void validate_shouldMarkClientAsFraud() throws Exception {
     // given:
-    MockMvcRequestSpecification request = given()
-        .header("Content-Type", "application/json")
-        .body("{\"description\":\"I am a simple description\",\"completed\":true}");
+//    MockMvcRequestSpecification request = given()
+//        .header("Content-Type", "application/json")
+//        .body("{\"description\":\"I am a simple description\",\"completed\":true}");
+    MockMvcResponse request = given()
+        .when()
+        .get("https://ocean-rest-test.herokuapp.com/items");
 
     // when:
-    ResponseOptions response = given().spec(request)
+    ResponseOptions response = given().when()
         .get("https://ocean-rest-test.herokuapp.com/items");
 
     // then:
-    assertThat(String.valueOf(response.statusCode())).isEqualTo(200);
-    //assertThat(response.statusCode()).isEqualTo(200);
-    assertThat(response.header("Content-Type")).isEqualTo("application/json");
+    //assertThat(String.valueOf(response.statusCode())).isEqualTo(200);
+//    assertThat(response.header("Content-Type"));
+    //assertThat(response.header("Content-Type")).isEqualTo("application/json");
     // and:
-    DocumentContext parsedJson = JsonPath.parse(response.getBody().asString());
-    assertThatJson(parsedJson).field("description").isEqualTo("FRAUD");
-    assertThatJson(parsedJson).field("completed").isEqualTo("Amount too high");
+    DocumentContext parsedJson = JsonPath.parse(response.statusCode());
+    //assertThatJson(parsedJson).field("description").isEqualTo("Result of successful REST call!!");
+    assertThatJson(parsedJson).field("description").isEqualTo("hello");
+//    assertThatJson(parsedJson).field("completed").isEqualTo(true);
   }
 
 }
