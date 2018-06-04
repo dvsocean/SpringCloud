21da46f25b21c24ae0720327c2b5512bf8768d25
Ocean <dvsocean@icloud.com>
2018-06-04 10:49:46 -0700

    mods before service re-create

--- src/main/java/hello/ContractRestServiceApplication.java
+++ src/main/java/hello/ContractRestServiceApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContractRestServiceApplication {
 
-  //NOT SURE WHY I NEED A MAIN METHOD IF IM EXECUTING FROM THE TERMINAL..STILL WORKS WITHOUT IT
-//  public static void main(String[] args) {
-//    SpringApplication.run(ContractRestServiceApplication.class, args);
-//  }
+  public static void main(String[] args) {
+    SpringApplication.run(ContractRestServiceApplication.class, args);
+  }
 
 }
